a2db783eb2ef931cde5c3b45bd9cc92b7994bdb6
ente <ducksource@duckpond.ch>
2016-08-09 19:52:07 +0200

    [wip] debug

--- xt_cert_finger_print.h
+++ xt_cert_finger_print.h
@@ -137,7 +137,7 @@ static ssize_t sslpin_read_finger_print(const char* buf, size_t count, sslpin_re
 
     // read finger prints
     while(buf + SSLPIN_FINGER_PRINT_STR_SIZE <= buf_end){
-        int ret = hex2bin(fp, buf, SSLPIN_FINGER_PRINT_STR_SIZE);
+        int ret = hex2bin(fp, buf, sizeof(fp));
         if(ret){
             pr_err("invalid finger print hex representation: %." STR(SSLPIN_FINGER_PRINT_STR_SIZE) "s\n", buf);
             goto err_invalid_hex_repr;
